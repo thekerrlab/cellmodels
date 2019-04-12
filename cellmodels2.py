b7248282e8130af601668ae9967ddd2ba17d0531
cliffckerr <questionably@gmail.com>
2019-04-12 16:23:28 +1000

    split up into izhi and h-h

--- cellmodels2.py
+++ cellmodels2.py
@@ -23,34 +23,45 @@ simConfig = specs.SimConfig()   # object of class SimConfig to store the simulat
 # NETWORK PARAMETERS
 ###############################################################################
 
+
+which = 'h-h'
+
 # Population parameters
-netParams.popParams['PYR_HH'] = {'cellModel': 'HH', 'cellType': 'PYR', 'numCells': 50} # add dict with params for this pop 
-netParams.popParams['PYR_Izhi'] = {'cellModel': 'Izhi', 'cellType': 'PYR', 'numCells': 50} # add dict with params for this pop 
+if which == 'izhi':
+    netParams.popParams['PYR'] = {'cellModel': 'Izhi', 'cellType': 'PYR', 'numCells': 100} # add dict with params for this pop 
+elif which == 'h-h':
+    netParams.popParams['PYR'] = {'cellModel': 'HH', 'cellType': 'PYR', 'numCells': 100} # add dict with params for this pop 
+
 
 
 # Cell parameters list
-## PYR cell properties (HH)
-cellRule = {'conds': {'cellType': 'PYR', 'cellModel': 'HH'},  'secs': {}}
-cellRule['secs']['soma'] = {'geom': {}, 'topol': {}, 'mechs': {}}  # soma properties
-cellRule['secs']['soma']['geom'] = {'diam': 6.3, 'L': 5, 'Ra': 123.0, 'pt3d':[]}
-cellRule['secs']['soma']['geom']['pt3d'].append((0, 0, 0, 20))
-cellRule['secs']['soma']['geom']['pt3d'].append((0, 0, 20, 20))
-cellRule['secs']['soma']['mechs']['hh'] = {'gnabar': 0.12, 'gkbar': 0.036, 'gl': 0.003, 'el': -70} 
-
-cellRule['secs']['dend'] = {'geom': {}, 'topol': {}, 'mechs': {}}  # dend properties
-cellRule['secs']['dend']['geom'] = {'diam': 5.0, 'L': 150.0, 'Ra': 150.0, 'cm': 1, 'pt3d': []}
-cellRule['secs']['dend']['topol'] = {'parentSec': 'soma', 'parentX': 1.0, 'childX': 0}
-cellRule['secs']['dend']['mechs']['pas'] = {'g': 0.0000357, 'e': -70} 
-
-netParams.cellParams['PYR_HH'] = cellRule  # add dict to list of cell properties
 
 ## PYR cell properties (Izhi)
-cellRule = {'conds': {'cellType': 'PYR', 'cellModel': 'Izhi'},  'secs': {}}
-cellRule['secs']['soma'] = {'geom': {}, 'pointps':{}}  # soma properties
-cellRule['secs']['soma']['geom'] = {'diam': 10, 'L': 10, 'cm': 31.831}
-cellRule['secs']['soma']['pointps']['Izhi'] = {'mod':'Izhi2007b', 
-    'C':1, 'k':0.7, 'vr':-60, 'vt':-40, 'vpeak':35, 'a':0.03, 'b':-2, 'c':-50, 'd':100, 'celltype':1}
-netParams.cellParams['PYR_Izhi'] = cellRule  # add dict to list of cell properties
+if which == 'izhi':
+    cellRule = {'conds': {'cellType': 'PYR', 'cellModel': 'Izhi'},  'secs': {}}
+    cellRule['secs']['soma'] = {'geom': {}, 'pointps':{}}  # soma properties
+    cellRule['secs']['soma']['geom'] = {'diam': 10, 'L': 10, 'cm': 31.831}
+    cellRule['secs']['soma']['pointps']['Izhi'] = {'mod':'Izhi2007b', 
+        'C':1, 'k':0.7, 'vr':-60, 'vt':-40, 'vpeak':35, 'a':0.03, 'b':-2, 'c':-50, 'd':100, 'celltype':1}
+    netParams.cellParams['PYR'] = cellRule  # add dict to list of cell properties
+
+## PYR cell properties (HH)
+elif which == 'h-h':
+    cellRule = {'conds': {'cellType': 'PYR', 'cellModel': 'HH'},  'secs': {}}
+    cellRule['secs']['soma'] = {'geom': {}, 'topol': {}, 'mechs': {}}  # soma properties
+    cellRule['secs']['soma']['geom'] = {'diam': 6.3, 'L': 5, 'Ra': 123.0, 'pt3d':[]}
+    cellRule['secs']['soma']['geom']['pt3d'].append((0, 0, 0, 20))
+    cellRule['secs']['soma']['geom']['pt3d'].append((0, 0, 20, 20))
+    cellRule['secs']['soma']['mechs']['hh'] = {'gnabar': 0.12, 'gkbar': 0.036, 'gl': 0.003, 'el': -70} 
+    
+    cellRule['secs']['dend'] = {'geom': {}, 'topol': {}, 'mechs': {}}  # dend properties
+    cellRule['secs']['dend']['geom'] = {'diam': 5.0, 'L': 150.0, 'Ra': 150.0, 'cm': 1, 'pt3d': []}
+    cellRule['secs']['dend']['topol'] = {'parentSec': 'soma', 'parentX': 1.0, 'childX': 0}
+    cellRule['secs']['dend']['mechs']['pas'] = {'g': 0.0000357, 'e': -70} 
+    
+    netParams.cellParams['PYR'] = cellRule  # add dict to list of cell properties
+
+
 
 
 # Synaptic mechanism parameters
@@ -59,10 +70,12 @@ netParams.synMechParams['AMPA'] = {'mod': 'ExpSyn', 'tau': 0.1, 'e': 0}
 
 # Stimulation parameters
 netParams.stimSourceParams['bkg'] = {'type': 'NetStim', 'rate': 10, 'noise': 0.5}
-netParams.stimTargetParams['bg->PYR_Izhi'] = {'source': 'bkg', 'conds': {'cellType': 'PYR', 'cellModel': 'Izhi'}, 
+if which == 'izhi':
+    netParams.stimTargetParams['bg->PYR'] = {'source': 'bkg', 'conds': {'cellType': 'PYR', 'cellModel': 'Izhi'}, 
                                             'weight': 1, 'delay': 'uniform(1,5)', 'synMech': 'AMPA'}  
-netParams.stimTargetParams['bg->PYR_HH'] = {'source': 'bkg', 'conds': {'cellType': 'PYR', 'cellModel': 'HH'}, 
-                                            'weight': 1, 'synMech': 'AMPA', 'sec': 'dend', 'loc': 1.0, 'delay': 'uniform(1,5)'}
+elif which == 'h-h':
+    netParams.stimTargetParams['bg->PYR'] = {'source': 'bkg', 'conds': {'cellType': 'PYR', 'cellModel': 'HH'}, 
+                                                'weight': 1, 'synMech': 'AMPA', 'sec': 'dend', 'loc': 1.0, 'delay': 'uniform(1,5)'}
 
 
 # Connectivity parameters
@@ -114,7 +127,7 @@ simConfig.saveDpk = False # save to a .dpk pickled file
 # Analysis and plotting 
 simConfig.analysis['plotRaster'] = {'orderInverse': False} #True # Whether or not to plot a raster
 simConfig.analysis['plotTraces'] = {'include': [1,51]} # plot recorded traces for this list of cells
-simConfig.analysis['plotRatePSD'] = {'include': ['allCells', 'PYR_HH', 'PYR_Izhi'], 'smooth': 10} # plot recorded traces for this list of cells
+simConfig.analysis['plotRatePSD'] = {'include': ['allCells', 'PYR'], 'smooth': 10} # plot recorded traces for this list of cells
 
 # Run simulation
 sim.createSimulateAnalyze(netParams=netParams, simConfig=simConfig)  # create and simulate network
